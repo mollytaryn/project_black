128796ff5b6f20ceaac65e961b3518b4a94fb9d8
Molly Black <mollytaryn@gmail.com>
2017-02-09 16:52:46 -0800

    Add additional flash classes and update helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,10 +2,10 @@ module ApplicationHelper
 
   def flash_class(level)
     case level.to_sym
-    when :notice then "Flash-notice"
-    when :success then "Flash-success"
-    when :error then "Flash-error"
-    when :alert then "Flash-alert"
+    when :notice then "Flash Flash-notice"
+    when :success then "Flash Flash-success"
+    when :error then "Flash Flash-error"
+    when :alert then "Flash Flash-alert"
     end
   end
 
