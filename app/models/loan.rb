fa7919a980a0eaff911dda16d9f23fa9b9957760
Molly Black <mollytaryn@gmail.com>
2017-10-24 19:44:46 -0700

    Add loan validations to model

--- app/models/loan.rb
+++ app/models/loan.rb
@@ -2,4 +2,10 @@ class Loan < ActiveRecord::Base
 
   belongs_to :user
 
+  validates_length_of :name, maximum: 30
+  validates_presence_of :name, :principle_balance, :interest_rate, :minimum_payment_due
+  validates_numericality_of :principle_balance, :interest_rate, :minimum_payment_due
+
+  scope :total, -> { sum(:principle_balance) }
+
 end
