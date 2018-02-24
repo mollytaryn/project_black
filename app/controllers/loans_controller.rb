8e1a50fe2db87262f30cc951638f977fbccce66e
Molly Black <mollytaryn@gmail.com>
2018-02-24 15:01:36 -0800

    allow error message on date input

--- app/controllers/loans_controller.rb
+++ app/controllers/loans_controller.rb
@@ -15,11 +15,18 @@ class LoansController < ApplicationController
   private
 
   def complete_params
-    loan_params.merge(next_due_date: Date.new(params[:next_due_date][:year].to_i, params[:next_due_date][:month].to_i, params[:next_due_date][:day].to_i))
+    loan_params.merge(next_due_date: construct_date)
   end
 
   def loan_params
     params.require(:loan).permit(:name, :principle_balance, :interest_rate, :minimum_payment_due, :loan_category_id)
   end
 
+  def construct_date
+    begin
+      Date.new(params[:next_due_date][:year].to_i, params[:next_due_date][:month].to_i, params[:next_due_date][:day].to_i)
+    rescue
+      nil
+    end
+  end
 end
