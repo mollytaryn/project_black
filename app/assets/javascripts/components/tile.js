de3907dd47d18c06614a049bf5e5336d8c91f102
Molly Black <mollytaryn@gmail.com>
2018-05-05 16:36:17 -0700

    Iterate on the tile pattern & display on dashboard
    Why: So that it is more easily readible, and now has an active state.

--- app/assets/javascripts/components/tile.js
+++ app/assets/javascripts/components/tile.js
@@ -3,7 +3,10 @@
   var initTile = function () {
     var $tile = $('.js-Tile');
 
+    $tile.filter(':first').css('is-selected');
+
     $tile.on('click', function () {
+      $tile.removeClass('is-selected');
       $(this).toggleClass('is-selected');
     });
   };
