16244e7ffa6a659b09831ee0a7dc75d322f1a9e0
Molly Black <mollytaryn@gmail.com>
2018-06-09 13:39:11 -0700

    Make pay off date tile dynamic

--- app/assets/javascripts/base/amortization.js
+++ app/assets/javascripts/base/amortization.js
@@ -9,75 +9,97 @@
         balance = loanBalance;
 
     function interestThisMonth(balance) {
-      return balance * monthlyInterestRate
-    };
+      return balance * monthlyInterestRate;
+    }
 
     function monthlyAdditionalPayment() {
       return ($monthlyAdditionalPayment.val().length ? $monthlyAdditionalPayment.val() : 0);
-    };
+    }
 
     function principalThisMonth(balance) {
-      return minPaymentDue - interestThisMonth(balance)
-    };
-
-    function round(value) {
-      return Math.round(100 * value)/100
-    };
+      return minPaymentDue - interestThisMonth(balance);
+    }
 
     function amortizationSchedule(balance, map) {
-      monthlyInterestPrincipalBalance = []
+      var monthlyInterestPrincipalBalance = [];
       while (parseInt(balance) > parseInt(minPaymentDue)) {
-        new_balance = balance - (principalThisMonth(balance) + parseInt(map))
-        monthlyInterestPrincipalBalance.push([round(interestThisMonth(balance)), round(principalThisMonth(balance)), round(new_balance)])
+        var new_balance = balance - (principalThisMonth(balance) + parseInt(map));
+        monthlyInterestPrincipalBalance.push([roundTwoDecimals(interestThisMonth(balance)), roundTwoDecimals(principalThisMonth(balance)), roundTwoDecimals(new_balance)]);
         balance = new_balance
       }
-      return monthlyInterestPrincipalBalance
-    };
+      return monthlyInterestPrincipalBalance;
+    }
     return amortizationSchedule(balance, monthlyAdditionalPayment());
-  };
+  }
 
   var calculateInterestDue = function () {
-    interestDue = 0
+    var interestDue = 0;
     $.each(amortizationSchedule(), function(i, v) {
-      interestDue += v[0]
+      interestDue += v[0];
     });
     return interestDue;
   };
 
-  var initSavedInterest = function (selected_loan) {
-    var $selectedLoan = (typeof selected_loan == 'undefined' ? $('.js-Tile, .is-selected') : selected_loan),
+  var initSavedMonths = function (selectedLoan) {
+    var $monthlyAdditionalPayment = $('.js-MonthlyAdditionalPayment'),
+        $payOffMonthTile = $('.js-PayOffMonthTile');
+
+    $monthlyAdditionalPayment.keyup(function () {
+      $payOffMonthTile.html(formatPayOffDate(amortizationSchedule().length));
+    });
+  };
+
+  var initSavedInterest = function (selectedLoan) {
+    var $selectedLoan = (typeof selectedLoan == 'undefined' ? $('.js-Tile, .is-selected') : selectedLoan),
         loanInterest = $selectedLoan.data('total-interest'),
         $interestSaved = $('.js-InterestSaved'),
         $monthlyAdditionalPayment = $('.js-MonthlyAdditionalPayment'),
-        $totalInterest = $('.js-TotalInterest'),
-        allLoanInterest = $totalInterest.data('all-loan-interest');
+        $interestTile = $('.js-InterestTile');
 
     $monthlyAdditionalPayment.keyup(function () {
       var interestSaved = (loanInterest - calculateInterestDue());
-      $interestSaved.html('$' + Math.round(interestSaved));
-      $totalInterest.html('$' + Math.round(allLoanInterest - interestSaved));
+      $interestSaved.html(formatCurrency(interestSaved));
+      $interestTile.html(formatCurrency(loanInterest - interestSaved));
     });
   };
 
   var initSelectedLoan = function () {
-    var $loans = $('.js-Tile'),
-        $monthlyAdditionalPayment = $('.js-MonthlyAdditionalPayment'),
-        $interestSaved = $('.js-InterestSaved'),
-        $totalInterest = $('.js-TotalInterest'),
-        allLoanInterest = $totalInterest.data('all-loan-interest');
+    var $loans = $('.js-Tile');
 
     $loans.on('click', function () {
       var $selectedLoan = $(this);
 
+      initSavedInterest($selectedLoan);
+      initSavedMonths();
+    });
+  };
+
+  var initSwitchLoanTiles = function () {
+    var $loans = $('.js-Tile'),
+        $monthlyAdditionalPayment = $('.js-MonthlyAdditionalPayment'),
+        $interestSaved = $('.js-InterestSaved'),
+        $interestTile = $('.js-InterestTile'),
+        $principalTile = $('.js-PrincipalTile'),
+        $payOffTile = $('.js-PayOffMonthTile');
+
+    $loans.on('click', function () {
+      var $selectedLoan = $(this),
+          loanInterest = $selectedLoan.data('total-interest'),
+          loanPrincipal = $selectedLoan.data('loan-balance'),
+          loanPayOffMonth = $selectedLoan.data('pay-off');
+
       $monthlyAdditionalPayment.val('');
       $interestSaved.html('$0');
-      $totalInterest.html('$' + Math.round(allLoanInterest));
-      initSavedInterest($selectedLoan);
+      $interestTile.html(formatCurrency(loanInterest));
+      $principalTile.html(formatCurrency(loanPrincipal));
+      $payOffTile.html(loanPayOffMonth);
     });
   };
 
   Black.Amortization = function () {
     initSavedInterest();
+    initSavedMonths();
     initSelectedLoan();
+    initSwitchLoanTiles();
   };
 }(this));
