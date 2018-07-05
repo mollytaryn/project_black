e895295bca142c8903c690f9a4182cd53b17d6d7
Molly Black <mollytaryn@gmail.com>
2018-07-05 17:05:53 -0500

    Update dashboard chart to use amortization data
    This change addresses the need by:
    - Style the chart
    - Pass chart first loan data on pg load

--- app/assets/javascripts/charts/dashboard_chart.js
+++ app/assets/javascripts/charts/dashboard_chart.js
@@ -1,41 +1,78 @@
 (function () {
 
   var initDashboardChart = function () {
-    var $container = $('.js-dashboardChart');
+    var $container = $('.js-dashboardChart'),
+        principalBalances = $container.data('principal-balances'),
+        monthsLeft = $container.data('months-array');
+
+    Chart.defaults.global.defaultFontColor = "#fff";
+    Chart.defaults.global.defaultFontFamily = "Montserrat";
 
     var dashboardChart = new Chart($container, {
       type: 'line',
       data: {
-        labels: ["January", "February", "March", "April", "May", "June", "July"],
-        datasets: [
+        labels: monthsLeft,
+        datasets: [{
+          label: "My First dataset",
+          data: principalBalances,
+          backgroundColor: "rgba(0,0,0,0)",
+          borderColor: "#2ac1ef",
+          borderWidth: 4,
+          lineTension: 0,
+          pointRadius: 0,
+          pointHitRadius: 5,
+          pointHoverBackgroundColor: "#2ac1ef",
+          pointHoverBorderColor: "#fff",
+          pointHoverBorderWidth: 3,
+          pointHoverRadius: 10,
+        },
         {
-            label: "My First dataset",
-            fill: false,
-            lineTension: 0.1,
-            backgroundColor: "rgba(75,192,192,0.4)",
-            borderColor: "rgba(75,192,192,1)",
-            borderCapStyle: 'butt',
-            borderDash: [],
-            borderDashOffset: 0.0,
-            borderJoinStyle: 'miter',
-            pointBorderColor: "rgba(75,192,192,1)",
-            pointBackgroundColor: "#fff",
-            pointBorderWidth: 1,
-            pointHoverRadius: 5,
-            pointHoverBackgroundColor: "rgba(75,192,192,1)",
-            pointHoverBorderColor: "rgba(220,220,220,1)",
-            pointHoverBorderWidth: 2,
-            pointRadius: 1,
-            pointHitRadius: 10,
-            data: [65, 59, 80, 81, 56, 55, 40],
-            spanGaps: false,
+          label: "My Second dataset",
+          data: principalBalances,
+          backgroundColor: "rgba(0,0,0,0)",
+          borderColor: "#fff",
+          borderDash: [5],
+          pointRadius: 0,
+          pointHoverBackgroundColor: "#000",
+          pointHoverBorderColor: "#fff",
+          pointHoverBorderWidth: 2,
+          pointHoverRadius: 6,
+        }]
+      },
+      options: {
+        scales: {
+          xAxes: [{
+            gridLines: {
+              display: false,
+              drawBorder: false
+            },
+            ticks: {
+              autoSkip: true,
+              maxTicksLimit: 12,
+              callback: function(value, index, values) {
+                return formatPayOffDate(value);
+              }
+            }
+          }],
+          yAxes: [{
+            gridLines: {
+              display: false,
+              drawBorder: false
+            },
+            ticks: {
+              callback: function(value, index, values) {
+                  return '$' + value;
+              }
+            }
+          }]
+        },
+        tooltips: {
+          intersect: false,
+          position: 'nearest',
+          mode: 'index',
+          axis: 'y'
         }
-        ]
       }
-    });
-
-    $container.hide().delay(600).slideDown('slow', function() {
-      $(this).delay(4500).fadeOut();
     });
   };
 
