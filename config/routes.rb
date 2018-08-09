3100f957644459cb326bf360c586787ce43d3e7b
Molly Black <mollytaryn@gmail.com>
2018-08-09 12:15:46 +0200

    Update assets and routes for rails 5

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'dashboard#show'
+
+  get 'home', to: 'dashboard#show', as: :current_user
+
+  resources :loans, only: [:new, :create, :index] do
+    collection do
+      get 'continue'
+    end
+  end
+
 end
